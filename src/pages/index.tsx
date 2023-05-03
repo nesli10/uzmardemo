7075809ce5ce232be4185dbee5bed791cb522976
Neslişah Ebral Durdu <nestay2001@gmail.com>
2023-05-03 09:15:16 +0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,33 +1,37 @@
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { selectAuthState, setAuthState } from "../store/authSlice";
-import {  setAuthUser } from "../store/authSlice";
+import { setAuthUser } from "../store/authSlice";
 import { useDispatch, useSelector } from "react-redux";
-import { useEffect} from "react";
-import { useRouter } from 'next/router';
-
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 function Copyright(props: any) {
   return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Copyright © '}
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
       <Link color="inherit" href="https://mui.com/">
         Your Website
-      </Link>{' '}
+      </Link>{" "}
       {new Date().getFullYear()}
-      {'.'}
+      {"."}
     </Typography>
   );
 }
@@ -37,38 +41,35 @@ export default function SignIn() {
   const authState = useSelector(selectAuthState);
   const dispatch = useDispatch();
   const router = useRouter();
-  if(authState){
-    router.push("/dashboard")
+  if (authState) {
+    router.push("/dashboard");
   }
 
-  const [email, setEmail] = React.useState('');
-  const [password, setPassword] = React.useState(''); 
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    fetch('/api/login', {
-      method: 'GET',
+    fetch("/api/login", {
+      method: "GET",
       headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Basic ${Buffer.from(`${email}:${password}`).toString(
-          'base64'
+        "Content-Type": "application/json",
+        Authorization: `Basic ${Buffer.from(`${email}:${password}`).toString(
+          "base64"
         )}`,
       },
-    }).then((res) => res.json().then((data) => {
-      console.log('Data =',data)
-      dispatch(setAuthState(data.loggedIn))
-      dispatch(setAuthUser(data.email));
-
-  })
+    }).then((res) =>
+      res.json().then((data) => {
+        dispatch(setAuthState(data.loggedIn));
+        dispatch(setAuthUser(data.email));
+      })
     );
-    
   };
-    useEffect(() => {
-      if (authState) {
-        router.push('/dashboard')
-        
-      }
-     }, [authState]);
-
+  useEffect(() => {
+    if (authState) {
+      dispatch({ type: "GET_ALL_TREE" });
+      router.push("/dashboard");
+    }
+  }, [authState]);
 
   return (
     <ThemeProvider theme={theme}>
@@ -77,20 +78,25 @@ export default function SignIn() {
         <Box
           sx={{
             marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
           }}
         >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
             Sign in
           </Typography>
-          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{ mt: 1 }}
+          >
             <TextField
-             type="email"
+              type="email"
               margin="normal"
               required
               fullWidth
@@ -123,10 +129,8 @@ export default function SignIn() {
               fullWidth
               variant="contained"
               sx={{ mt: 3, mb: 2 }}
-             
             >
               Sign In
-
             </Button>
             <Grid container>
               <Grid item xs>
