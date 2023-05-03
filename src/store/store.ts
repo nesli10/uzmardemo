14e37b0d58a5b2e317b8c561976fa00b02757636
Neslişah Ebral Durdu <nestay2001@gmail.com>
2023-05-03 09:16:33 +0300

    Update store.ts

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,16 +2,23 @@ import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
 import { authSlice } from "./authSlice";
 import { createWrapper } from "next-redux-wrapper";
 import { treeSlice } from "./treeSlice";
+import createSagaMiddleware from "redux-saga";
+import treeSaga from "./treeSaga";
 
-
-const makeStore = () =>
-  configureStore({
+const makeStore = () => {
+  const sagaMiddleware = createSagaMiddleware();
+  const store = configureStore({
     reducer: {
       [authSlice.name]: authSlice.reducer,
       [treeSlice.name]: treeSlice.reducer,
     },
     devTools: true,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware().concat(sagaMiddleware),
   });
+  sagaMiddleware.run(treeSaga);
+  return store;
+};
 
 export type AppStore = ReturnType<typeof makeStore>;
 export type AppState = ReturnType<AppStore["getState"]>;
